cf5da99d399ef07a5a0dbf18614ad65df25d659a
Technoswamp <technoswamp@gmail.com>
2023-02-23 20:31:36 -0600

    primer parcial

---
+++ app/src/main/java/com/example/splashanoemi/ApiPlus.java
@@ -0,0 +1,14 @@
+package com.example.splashanoemi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApiPlus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_plus);
+    }
+}
